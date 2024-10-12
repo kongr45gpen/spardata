d23c63f315915774014563f511c9d6123aa3d23e
kongr45gpen <electrovesta@gmail.com>
2024-10-12 02:08:36 +0200

    Update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,16 @@ export default defineNuxtConfig({
   runtimeConfig: {
     version: '0.0.1'
   },
-  // devtools: { enabled: true},
+
+  devtools: { enabled: true},
   modules: ['@nuxtjs/tailwindcss', 'nuxt-svgo', '@huntersofbook/naive-ui-nuxt'],
+
   extends: ['@sidebase/core'],
+
   typescript: {
     shim: false
   },
+
   app: {
     head: {
       bodyAttrs: {
@@ -20,9 +24,11 @@ export default defineNuxtConfig({
       htmlAttrs: {
         class: 'h-full'
       }
-    }
-    // rootAttrs: {
-    // class: 'h-full'
-    // },
-  }
+    },
+    rootAttrs: {
+      class: 'h-full'
+    },
+  },
+
+  compatibilityDate: '2024-10-12'
 })
